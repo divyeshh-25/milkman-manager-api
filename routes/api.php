a2c40b56af5bba8448257a11bab7da3e2f561bfe
divyeshh-25 <info.divyeshpanchal@gmail.com>
2025-09-07 18:24:51 +0530

    Feat: Milk Type APIs Done

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\MilkTypeController;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
@@ -13,4 +14,9 @@ Route::prefix('auth')->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/me', [AuthController::class, 'me']);
     });
+});
+
+
+Route::prefix('v1')->middleware('auth:sanctum')->group(function(){
+    Route::apiResource('milk-type',MilkTypeController::class);
 });
